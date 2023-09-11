ee66e9eba35e5086dee45a50a4433fe08e2f51ea
rukkya <rokiaossama@gmail.com>
2023-09-11 07:39:11 -0700

    would this work?

--- exe.c
+++ exe.c
@@ -9,8 +9,10 @@
 
 int exe(char *buff)
 {
+	pid_t pid;
 	int i = 0;
-	char *buff_cp, *argv[64], *token = strtok(buff_cp, " ");
+	int status;
+	char *argv[64], *token = strtok(buff, " ");
 
 	if (token == NULL)
 		return (1);
@@ -20,10 +22,22 @@ int exe(char *buff)
 		token = strtok(NULL, " ");
 	}
 	argv[i] = NULL;
-
-	if (execve(argv[0], argv, NULL) == -1)
-	{
-		perror("Error:");
+	pid=fork();
+	if(pid==-1){
+	exit(1);
 	}
+	if(pid==0){
+	if(execve(argv[0],argv,NULL)==-1){
+	perror("no command");
+	exit(127);
+	}
+	else{
+/*	printf(promp);*/
+	wait(&status);
+	}
+	}
+
+
+	
 	return (0);
 }
